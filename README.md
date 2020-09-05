1b80eec1b7c32a77d0eefb9ac54f7e77201cb020
aaroexxt <aaron.becker@gmail.com>
2020-09-04 22:01:43 -0700

    upload all info

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # BathroomIndicator
  How do you know if someone's in the bathroom? A custom PCB, of course...
+
+## Design Intent
+I built this mostly to practice my PCB design and layout skills, but it's actually quite cool in person. Basically, it's designed to be a comprehensive system for people who have to share space to have a way to tell others that they're busy. I've installed a set of these on a bathroom I share with my sister (who thought that was a good idea?) so that we can both know when somebody else is using the bathroom.
+
+## Pictures
+
+Version 1
+![Version 1](/Pictures/V1.JPG)
+
+
+Version 2
+![Version 2](/Pictures/V2.JPG)
+
+## Renderings
+
+Top - Version 2
+![Version 2 Top](/Pictures/V2Top.png)
+
+Bottom - Version 2
+![Version 2 Bottom](/Pictures/V2Bottom.png)
+
+### Design Notes
+
+The major difference between versions 1 and 2 is that Version 1 had a custom radio based on the NRF24L01+ radio by Nordic Semiconductor, while this was replaced with a premade module in Version 2 because of my lack of experience with antenna design (1 foot range on my custom one vs _30_ feet on the premade module). Other than that, it worked well. I also had some issues with the 3.3v regulator schematic in V1 (incorrect schematic for EN pin) but this was solved in version 2.
+
+### By Aaron Becker
---
+++ Software/indicatorB/indicatorB.ino
@@ -0,0 +1,228 @@
+//IndicatorB.ino by Aaron Becker
+
+#include <Arduino_APDS9960.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <avr/power.h>
+
+RF24 radio(7, 8); // CE, CSN
+const byte addresses[][6] = {"00002", "00003"};
+//TODO sleep mode and IRQ pin interrupts
+#define PROX_THRESH 20
+#define CLEAR 5
+#define BUSY 6
+#define IRQ 2
+
+const boolean debug = false;
+
+boolean radioListening = false;
+void setup() {
+	Serial.begin(9600);
+	while (!Serial); // Wait for serial monitor to open
+
+	boolean error = false;
+	int errorDelay = 100;
+	int okDelay = 50;
+	if (!APDS.begin()) {
+		Serial.println("Error initializing APDS9960 sensor.");
+		error = true;
+		errorDelay = 50;
+	}
+	APDS.setGestureSensitivity(85);
+
+	//Power saving stuff
+	power_adc_disable();
+	power_usart0_disable();
+
+	//Radio stuff
+  	radio.begin();
+	radio.setPALevel(RF24_PA_MAX); //max because we don't want to lose connection
+	radio.setRetries(3,3); // delay, count
+	radio.openWritingPipe(addresses[1]);
+	radio.openReadingPipe(1, addresses[0]); //set address to recieve data
+	radio.maskIRQ(1,1,0);
+	attachInterrupt(digitalPinToInterrupt(IRQ), recv, FALLING);
+	radioTransmitMode();
+	radio.stopListening();
+
+	  pinMode(CLEAR, OUTPUT);
+	  pinMode(BUSY, OUTPUT);
+
+	while (error) {
+		analogWrite(CLEAR, 32);
+		digitalWrite(BUSY, LOW);
+		delay(errorDelay);
+		digitalWrite(CLEAR, LOW);
+		analogWrite(BUSY, 32);
+		delay(errorDelay);
+	}
+
+	Serial.println("IndicatorA starting");
+
+	analogWrite(CLEAR, 32);
+	digitalWrite(BUSY, LOW);
+	delay(okDelay);
+	digitalWrite(CLEAR, LOW);
+	analogWrite(BUSY, 32);
+	delay(okDelay);
+	analogWrite(CLEAR, 32);
+	digitalWrite(BUSY, LOW);
+	delay(okDelay);
+	digitalWrite(CLEAR, LOW);
+	analogWrite(BUSY, 32);
+	delay(okDelay);
+	digitalWrite(CLEAR, LOW);
+	digitalWrite(BUSY, LOW);
+	delay(500);
+
+}
+
+int proximity = 0;
+unsigned long lastUpdate = 0;
+int currentState = 0;
+int roomClear = 0;
+boolean toggle = false;
+
+unsigned long lastRecvTime = 0;
+unsigned long lastStatusMillis = 0;
+
+void loop() {
+	switch (currentState) {
+		case 0:
+			if (millis() - lastUpdate > 100) {
+				if (!toggle) {
+					analogWrite(CLEAR, 32);
+					analogWrite(BUSY, 32);
+					toggle = true;
+				} else {
+					digitalWrite(CLEAR, LOW);
+					digitalWrite(BUSY, LOW);
+					toggle = false;
+				}
+				send(200);
+				lastUpdate = millis();
+			}
+			break;
+		case 1:
+			if (proximity < PROX_THRESH) {
+				if (roomClear) {
+					analogWrite(CLEAR, 32);
+					digitalWrite(BUSY, LOW);
+				} else {
+					digitalWrite(CLEAR, LOW);
+					analogWrite(BUSY, 32);
+				}
+				lastUpdate = millis();
+			} else { //maybe add a thing where u cant swipe after taking hand away
+				digitalWrite(CLEAR, LOW);
+				digitalWrite(BUSY, LOW);
+				toggle = true;
+			}
+
+			if (millis() - lastUpdate > 1000 && toggle) {
+				digitalWrite(CLEAR, LOW);
+				digitalWrite(BUSY, LOW);
+				toggle = false;
+			}
+			break;
+	}
+
+	// Check if a proximity reading is available.
+	if (APDS.proximityAvailable()) {
+		proximity = APDS.readProximity();
+	}
+
+	// check if a gesture reading is available
+	if (APDS.gestureAvailable()) {
+		int gesture = APDS.readGesture();
+		switch (gesture) {
+		  case GESTURE_UP:
+		    roomClear = 1;
+		    send(roomClear);
+		    lights(true,2);
+		    break;
+
+		  case GESTURE_DOWN:
+		    roomClear = 0;
+		    send(roomClear);
+		    lights(false,2);
+		    break;
+		}
+	}
+	if (millis() - lastStatusMillis > 1500) {
+		send(200);
+		if (millis() - lastRecvTime >= 4700) {
+			currentState = 0;
+		}
+		lastStatusMillis = millis();
+	}
+}
+
+void send(int dat) {
+	radioTransmitMode();
+	/*if (dat != 200) {
+		Serial.print("S: ");
+		Serial.println(dat);
+	}*/
+    radio.write(&dat, sizeof(dat));
+    radioRecieveMode();
+}
+
+void recv() {
+	radioRecieveMode();
+	if (radio.available()) {
+		int med = 0;
+		radio.read(&med, sizeof(med));
+
+		/*if (med != 200) {
+			Serial.print("R: ");
+			Serial.println(med);
+		}*/
+		if (med < 200) {
+			roomClear = med;
+		}
+		lastRecvTime = millis();
+		currentState = 1;
+	}
+}
+
+void radioRecieveMode() {
+  if (!radioListening) { //if we're not listening
+    radio.startListening();
+    radioListening = true;
+  }
+}
+
+void radioTransmitMode() {
+  if (radioListening) {
+    radio.stopListening();
+    radioListening = false;
+  }
+}
+
+void lights(boolean state, int count) {
+	send(roomClear);
+	if (state) {
+		analogWrite(CLEAR, 32);
+		digitalWrite(BUSY, LOW);
+	} else {
+		digitalWrite(CLEAR, LOW);
+		analogWrite(BUSY, 32);
+	}
+	send(roomClear);
+
+	for (int i = 0; i<count; i++) {
+		delay(100);
+		digitalWrite(CLEAR, LOW);
+		digitalWrite(BUSY, LOW);
+		delay(100);
+		if (state) {
+			analogWrite(CLEAR, 32);
+			digitalWrite(BUSY, LOW);
+		} else {
+			digitalWrite(CLEAR, LOW);
+			analogWrite(BUSY, 32);
+		}
+	}
+}
